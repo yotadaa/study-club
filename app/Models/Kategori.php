6cbb4c282f4330da3cb0f5957f2a54164bb82d8b
yotadaa <mukhtadanasution@gmail.com>
2024-01-24 08:36:41 +0700

    second in third

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
-    use HasFactory;
+
+    protected $table = 'kategori';
+    protected $primaryKey = 'id';
+    protected $fillable = ['nama_kategori','deskripsi'];
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     return view('index');
 });
+Route::get('/dashboard', function() {
+    return view('index');
+});
