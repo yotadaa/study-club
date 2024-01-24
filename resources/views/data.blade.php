82b934b43733d8b8f4c8de07def58d7fa0d1264b
yotadaa <mukhtadanasution@gmail.com>
2024-01-24 12:20:28 +0700

    another commit

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -12,16 +12,61 @@
         <h1 class='text-3xl font-semibold mb-0'>
             Daftar Mahasiswa
         </h1>
+        <form>
+            <table class="mb-0 mt-10">
+                <tr class="bg-gray-200">
+                    <td>
+                        <input
+                            placeholder='NIM'
+                            type='text'
+                            class='border-none focus:outline-none bg-none outline-none'
+                            required
+                        >
+                    </td>
+                    <td>
+                        <input
+                            placeholder='Nama Mahasiswa'
+                            type='text'
+                            class='border-none focus:outline-none'
+                            required
+                        >
+                    </td>
+                    <td>
+                        <input
+                            placeholder='Deskripsi'
+                            type='text'
+                            class='border-none focus:outline-none'
+                            required
+                        >
+                    </td>
+                    <td>
+                        <button class="p-2 flex items-center justify-center bg-emerald-500 shadow-md rounded-md px-4">Tambah</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
         <table class="mt-0">
             @foreach($mhs as $data)
                 @if( $loop->first or $loop->iteration  >= 2 )
-                    <tr class="mt-0 bg-blue-500 rounded-full hover:cursor-pointer text-white w-10 h-10 mb-10 hover:opacity-80">
-                        <td class="p-2 bg-emerald-500">
-                            {{$data->nim}}
-                        </td>
-                        <td class='p-2' >{{$data->nama}}</td>
-                        <td class='p-2 bg-red-700 '>{{$data->des}}</td>
-                    </tr>
+                    @if ($loop->iteration % 2 != 0)
+                        <tr class="mt-0 bg-blue-500 rounded-full hover:cursor-pointer text-white w-10 h-10 mb-10 hover:opacity-80">
+                            <td>&times;</td>
+                            <td class="p-2 bg-emerald-500">
+                                {{$data->nim}}
+                            </td>
+                            <td class='p-2' >{{$data->nama}}</td>
+                            <td class='p-2 bg-red-700 '>{{$data->des}}</td>
+                        </tr>
+                    @else
+                        <tr class="mt-0 bg-blue-600 rounded-full hover:cursor-pointer text-white w-10 h-10 mb-10 hover:opacity-80">
+                            <td>&times;</td>
+                            <td class="p-2 bg-emerald-600">
+                                {{$data->nim}}
+                            </td>
+                            <td class='p-2' >{{$data->nama}}</td>
+                            <td class='p-2 bg-red-800 '>{{$data->des}}</td>
+                        </tr>
+                    @endif
                     <br>
                 @endif
             @endforeach
